68a31265b8feca2096d5ed3d4b26b3c24750c228
Mikhail Repin <migel72333@gmail.com>
2021-02-21 02:09:24 +0300

    build_dist

---
+++ precache-manifest.416a09aaea91ae2b0721770bc7d4024d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272ceb2032a608bd8c18",
+    "url": "/webapp/css/app.61ebb985.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/webapp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9784b607adbcfc86660e690f8f1da547",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "9c997779ad3c8af1009d",
+    "url": "/webapp/js/about.4554c9b3.js"
+  },
+  {
+    "revision": "272ceb2032a608bd8c18",
+    "url": "/webapp/js/app.b42bcadc.js"
+  },
+  {
+    "revision": "a71e62ed12b5da23b4b5",
+    "url": "/webapp/js/chunk-vendors.6bca2ecc.js"
+  },
+  {
+    "revision": "ad64b18fd4ea1b02a1205eac04470b9e",
+    "url": "/webapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/webapp/robots.txt"
+  }
+]);
